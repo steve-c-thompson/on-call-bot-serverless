55f2d8b7fae56ef8c7b3e49142a51606a21175e3
Steve Thompson <scthomps312@gmail.com>
2022-01-15 12:02:06 -0700

    Updates to improve webpack, and use async method with global promise for initialization.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {App as BoltApp, AwsLambdaReceiver, LogLevel} from '@slack/bolt';
+import {App, AwsLambdaReceiver, LogLevel} from '@slack/bolt';
 import {SlackBot} from "./bot/SlackBot";
 import {RespondArguments} from "@slack/bolt/dist/types/utilities";
 import {APIGatewayProxyEvent} from "aws-lambda";
@@ -6,31 +6,31 @@ import {GoogleSheetScheduleDataLoader} from "./data/GoogleSheetScheduleDataLoade
 import {ScheduleData} from "./model/ScheduleData";
 import {AwsSecretsDataSource} from "./secrets/AwsSecretsDataSource";
 import {context} from "./secrets/utils";
-import 'source-map-support/register';
 
-
-require('dotenv').config({ path: require('find-config')('.env') })
-
-let boltApp: BoltApp;
-// const dataSource = new EnvSecretDataSource();
+let app: App;
 const dataSource = new AwsSecretsDataSource(context.secretsManager);
 
-// Initialize by executing async function on startup. This is due to promised being returned from the dataSource, and
-// needing to wait for those in ctors.
-(async () => {
-    receiver = new AwsLambdaReceiver({
-        signingSecret:  await dataSource.signingSecret(),
+let awsLambdaReceiver: AwsLambdaReceiver;
+
+let slackBot : SlackBot;
+let dataLoaderSheet : GoogleSheetScheduleDataLoader;
+
+const init = async () => {
+    // console.log("Executing async init");
+    const signingSecret = await dataSource.signingSecret();
+    const slackBotToken = await dataSource.slackBotToken()
+    awsLambdaReceiver = new AwsLambdaReceiver({
+        signingSecret: signingSecret,
+        logLevel: LogLevel.DEBUG
     });
-    boltApp = new BoltApp({
-        token: await dataSource.slackBotToken(),
-        receiver: receiver,
-        // processBeforeResponse: true
+    app = new App({
+        token: slackBotToken,
+        receiver: awsLambdaReceiver,
         logLevel: LogLevel.DEBUG,
     });
 
-    boltApp.command("/oncall", async ({ command, ack, respond }) => {
-        // Acknowledge command request
-        console.log("Handling /oncall command " + command.text);
+    app.command("/oncall", async ({ command, ack, respond }) => {
+        // console.log("Handling /oncall command " + command.text);
         await ack();
 
         let args = command.text;
@@ -73,85 +73,44 @@ const dataSource = new AwsSecretsDataSource(context.secretsManager);
             attachments: attachments
         });
     });
-})()
 
-let receiver: AwsLambdaReceiver;
-
-let slackBot : SlackBot;
-let dataLoaderSheet : GoogleSheetScheduleDataLoader;
+    slackBot = await initBot();
+    return await awsLambdaReceiver.start();
+}
 
 /**
  * Init function to load the sheet data and create a new slackBot
  * Runs only if a slackBot has not been created.
  */
-async function init() {
+const initBot = async(): Promise<SlackBot> => {
     if(!slackBot) {
         const googleSheetId = await dataSource.googleSheetId();
         console.log("SLACK_GOOGLE_SHEET_ID: " + googleSheetId);
         dataLoaderSheet = new GoogleSheetScheduleDataLoader(googleSheetId);
         const scheduleData = await loadSheet(dataLoaderSheet);
-        slackBot = new SlackBot(scheduleData);
+        return new SlackBot(scheduleData);
+    }
+    else {
+        return slackBot;
     }
 }
 
-
-async function asyncDataRefresh(slackBot: SlackBot, dataLoaderSheet: GoogleSheetScheduleDataLoader) {
+const asyncDataRefresh = async (slackBot: SlackBot, dataLoaderSheet: GoogleSheetScheduleDataLoader) => {
     const scheduleData = await loadSheet(dataLoaderSheet);
     await slackBot.refresh(scheduleData);
 }
 
-// function writeToChannels(message: string, res: Response) {
-//     // write to channels - be sure to add the bot to the channels or this will fail
-//     const ids = slackBot.getSlackChannelIds();
-//     let requests = ids.map(id => {
-//         return new Promise((resolve, reject) => {
-//             boltApp.client.chat.postMessage({
-//                 channel: id,
-//                 text: message
-//             }).then(result => {
-//                 if (result.error) {
-//                     reject(result.error);
-//                 } else {
-//                     resolve(result.message);
-//                 }
-//             }).catch(e => {
-//                 reject(e);
-//             });
-//
-//         });
-//     });
-//     Promise.all(requests)
-//         .then(() => {
-//             res.send("Success");
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.send("Error in call - check the logs");
-//             return err;
-//         });
-//
-// }
-// receiver.router.post("/oncall-now", (req, res) => {
-//     // write to channels - be sure to add the bot to the channels or this will fail
-//     slackBot.handleNowRequest().then(msg => {
-//         writeToChannels(msg, res);
-//     }).catch((e: Error) => {
-//         res.send("Error in call - check the logs");
-//         console.log(e);
-//     })
-// });
-
-async function loadSheet(dataLoaderSheet: GoogleSheetScheduleDataLoader) : Promise<ScheduleData> {
+const loadSheet = async (dataLoaderSheet: GoogleSheetScheduleDataLoader) : Promise<ScheduleData> => {
     return dataLoaderSheet.init({
         accountEmail: await dataSource.googleServiceAccountEmail(),
         privateKey: await dataSource.googlePrivateKey()
     });
 }
 
+const initPromise = init();
+
 // Handle the Lambda function event
 module.exports.handler = async (event:APIGatewayProxyEvent, context:any, callback:any) => {
-    await init();
-    console.log("Starting receiver...");
-    const handler = await receiver.start();
+    const handler = await initPromise;
     return handler(event, context, callback);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = (async () => {
     return {
         mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-        devtool: 'eval-source-map',
+        devtool: 'eval-cheap-module-source-map',
         entry: slsw.lib.entries,
         externals: [nodeExternals()],
         optimization: {
